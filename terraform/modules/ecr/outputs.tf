c00c5bdb24a5f5beee3240e6ed066269c4c00d1f
Daniel Berliant <berliantdaniel@gmail.com>
2025-08-11 16:47:32 +0300

    Fixed user_data script, working EC2 VM with ECR docker container run

--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -2,3 +2,7 @@ output "ecr_repo_url" {
   description = "URL of the ECR repository"
   value       = aws_ecr_repository.myHotel.repository_url
 }
+
+output "build_and_push_done" {
+  value = null_resource.docker_build_and_push.id
+}
