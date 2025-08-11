2248e637a91d20c1154018949134d301486316a1
Daniel Berliant <berliantdaniel@gmail.com>
2025-08-11 15:01:53 +0300

    Added EC2 user_data to run the ECR - still requires testing

--- terraform/main/outputs.tf
+++ terraform/main/outputs.tf
@@ -1,3 +1,9 @@
 output "ecr_url" {
   value = module.myHotel_APP_ECR.ecr_repo_url
 }
+
+
+output "ec2_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.hotel_ec2.public_ip
+}
