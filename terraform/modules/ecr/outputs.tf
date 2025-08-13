1652228532969008dcea1bb3cca4e3f2f6aac853
Daniel Berliant <berliantdaniel@gmail.com>
2025-08-13 08:34:02 +0300

    Added Load balancer, Auto scaler and Target group module

--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -6,3 +6,11 @@ output "ecr_repo_url" {
 output "build_and_push_done" {
   value = null_resource.docker_build_and_push.id
 }
+
+output "alb_dns_name" {
+  value = aws_lb.this.dns_name
+}
+
+output "alb_arn" {
+  value = aws_lb.this.arn
+}
