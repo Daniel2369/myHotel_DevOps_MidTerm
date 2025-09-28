9043528a04714e1346cd912f95912a30584f72d4
Daniel2369 <120591150+Daniel2369@users.noreply.github.com>
2025-09-28 12:07:32 +0300

    Update test_backend.py

--- test_backend.py
+++ test_backend.py
@@ -2,6 +2,7 @@ import backend as backend
 import pytest
 
 def test_backend_initialization():
+    backend.setup_rooms()
     assert isinstance(backend.room_db, dict)
     assert len(backend.room_db) == 20
     
